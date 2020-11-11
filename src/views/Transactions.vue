3f479b755ef5e87eab6804e58e443b26676faad5
Patrick Tarnaud <patrick.tarnaud@gmail.com>
2020-11-11 11:26:54 +0100

    display selected account transactions from store

--- src/views/Transactions.vue
+++ src/views/Transactions.vue
@@ -1,16 +1,40 @@
 <template>
-    <p1>Transactions {{ $route.params.accountId }}</p1>
-    <!-- <v-data-table :headers="headers" :items="transactions" class="elevation-1">
-    </v-data-table> -->
+  <v-data-table :headers="headers" :items="transactions" class="elevation-1" @click:row="transactionSelected">
+    <!-- date format -->
+    <template v-slot:[`item.date`]="{ item }">
+      <span>{{ item.date.toLocaleString() }}</span>
+    </template>
+  </v-data-table>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   name: 'Transactions',
   data: () => {
     return {
-      headers: [{ text: 'Date' }, { text: 'Somme' }, { text: 'Nom' }, { text: 'Memo' }],
+      headers: [
+        { text: 'Date', value: 'date', dataType: 'Date' },
+        { text: 'Somme', value: 'amount' },
+        { text: 'Nom', value: 'name' },
+        { text: 'Memo', value: 'memo' },
+      ],
     }
   },
+  computed: {
+    ...mapState(['accounts']),
+
+    transactions: function() {
+      return this.accounts.filter(account => account.id === this.$route.params.accountId)[0].transactions
+    }
+  },
+  methods: {
+    transactionSelected: function(transaction) {
+      console.log('transaction: ', transaction)
+    }
+  }
 }
 </script>
+<style>
+</style>
